a3f990a74c7a15348cf52c9282c04f394b018391
Greyson Frazier <greyson.frazier@gmail.com>
2019-01-24 21:28:03 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from:'amanda',
-        text: 'Congrats!'
-    });
 });
 
 socket.on('disconnect', function() {
